80d4ccdd98df29b222c4855ba2a0f1a65947b123
Ramesh <102596577+rameshgiri0223@users.noreply.github.com>
2024-11-03 21:06:46 +0530

    Create FlightBookingApplication.java

---
+++ Flight Booking App/FlightBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.FlightBookingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingAppApplication.class, args);
+	}
+
+}
